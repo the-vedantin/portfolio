e0247770d6cc601b564961a234fe73246ebf6662
Aayu Yadav <aayuyadav25@gmail.com>
2026-05-21 19:14:22 -0500

    Add files via upload

---
+++ renders/manifest.js
@@ -0,0 +1,182 @@
+window.portfolioRenders = [
+  {
+    "sourceName": "0._Robot_(1)_Horus_2024-Apr-10_09-17-59PM-000_CustomizedView7474688316_jpg.jpg",
+    "local": "assets/renders/01-0-robot-1-horus-2024-apr-10-09-17-59pm-000-customizedview7474688316-jpg.jpg",
+    "bytes": 618267
+  },
+  {
+    "sourceName": "0._Robot_(1)_Horus_2024-Aug-08_10-46-18PM-000_CustomizedView5174201726_jpg.jpg",
+    "local": "assets/renders/02-0-robot-1-horus-2024-aug-08-10-46-18pm-000-customizedview5174201726-jpg.jpg",
+    "bytes": 1301429
+  },
+  {
+    "sourceName": "0._Robot_DISARM2_2024-Dec-26_06-57-51PM-000_CustomizedView25227543542_jpg.jpg",
+    "local": "assets/renders/03-0-robot-disarm2-2024-dec-26-06-57-51pm-000-customizedview25227543542-jpg.jpg",
+    "bytes": 257139
+  },
+  {
+    "sourceName": "0._Robot_DISARM2_2024-Dec-26_07-58-50PM-000_CustomizedView4611892363_jpg.jpg",
+    "local": "assets/renders/04-0-robot-disarm2-2024-dec-26-07-58-50pm-000-customizedview4611892363-jpg.jpg",
+    "bytes": 421526
+  },
+  {
+    "sourceName": "2583._LAYUP_WCPCADATHON_2024-Oct-10_08-36-00PM-000_CustomizedView8419476374_jpg.jpg",
+    "local": "assets/renders/05-2583-layup-wcpcadathon-2024-oct-10-08-36-00pm-000-customizedview84194763.jpg",
+    "bytes": 259279
+  },
+  {
+    "sourceName": "2583._LAYUP_WCPCADATHON_2024-Oct-10_08-58-02PM-000_CustomizedView4410554117_jpg.jpg",
+    "local": "assets/renders/06-2583-layup-wcpcadathon-2024-oct-10-08-58-02pm-000-customizedview44105541.jpg",
+    "bytes": 902620
+  },
+  {
+    "sourceName": "2583._LAYUP_WCPCADATHON_2024-Oct-10_09-13-19PM-000_CustomizedView13906047060_jpg.jpg",
+    "local": "assets/renders/07-2583-layup-wcpcadathon-2024-oct-10-09-13-19pm-000-customizedview13906047.jpg",
+    "bytes": 379974
+  },
+  {
+    "sourceName": "2583._LAYUP_WCPCADATHON_2024-Oct-10_09-15-18PM-000_CustomizedView36132625607_jpg.jpg",
+    "local": "assets/renders/08-2583-layup-wcpcadathon-2024-oct-10-09-15-18pm-000-customizedview36132625.jpg",
+    "bytes": 276172
+  },
+  {
+    "sourceName": "2583._LAYUP_WCPCADATHON_2024-Oct-12_12-38-55AM-000_CustomizedView18739970434_jpg.jpg",
+    "local": "assets/renders/09-2583-layup-wcpcadathon-2024-oct-12-12-38-55am-000-customizedview18739970.jpg",
+    "bytes": 559142
+  },
+  {
+    "sourceName": "808_2024-May-09_03-37-23AM-000_CustomizedView29669592357_jpg.jpg",
+    "local": "assets/renders/10-808-2024-may-09-03-37-23am-000-customizedview29669592357-jpg.jpg",
+    "bytes": 305840
+  },
+  {
+    "sourceName": "808_2024-May-09_03-50-51AM-000_CustomizedView30931609588_jpg.jpg",
+    "local": "assets/renders/11-808-2024-may-09-03-50-51am-000-customizedview30931609588-jpg.jpg",
+    "bytes": 341071
+  },
+  {
+    "sourceName": "808_2024-May-09_04-01-34AM-000_CustomizedView30931609588_jpg.jpg",
+    "local": "assets/renders/12-808-2024-may-09-04-01-34am-000-customizedview30931609588-jpg.jpg",
+    "bytes": 578046
+  },
+  {
+    "sourceName": "808_2024-Sep-01_06-05-54PM-000_CustomizedView27023079462_jpg.jpg",
+    "local": "assets/renders/13-808-2024-sep-01-06-05-54pm-000-customizedview27023079462-jpg.jpg",
+    "bytes": 398981
+  },
+  {
+    "sourceName": "808_2025-May-04_03-46-26AM-000_CustomizedView54175876698_jpg.jpg",
+    "local": "assets/renders/14-808-2025-may-04-03-46-26am-000-customizedview54175876698-jpg.jpg",
+    "bytes": 403593
+  },
+  {
+    "sourceName": "Flexicution_2024-Jun-10_10-49-04PM-000_CustomizedView26514002237_jpg.jpg",
+    "local": "assets/renders/15-flexicution-2024-jun-10-10-49-04pm-000-customizedview26514002237-jpg.jpg",
+    "bytes": 347826
+  },
+  {
+    "sourceName": "Flexicution_2024-Jun-10_11-13-00PM-000_CustomizedView23966177115_jpg.jpg",
+    "local": "assets/renders/16-flexicution-2024-jun-10-11-13-00pm-000-customizedview23966177115-jpg.jpg",
+    "bytes": 524380
+  },
+  {
+    "sourceName": "Flexicution_2024-Jun-10_11-13-21PM-000_CustomizedView42788883637_jpg.jpg",
+    "local": "assets/renders/17-flexicution-2024-jun-10-11-13-21pm-000-customizedview42788883637-jpg.jpg",
+    "bytes": 484191
+  },
+  {
+    "sourceName": "Horusv2_2024-Jul-30_03-20-53AM-000_CustomizedView4865332033_jpg.jpg",
+    "local": "assets/renders/18-horusv2-2024-jul-30-03-20-53am-000-customizedview4865332033-jpg.jpg",
+    "bytes": 309666
+  },
+  {
+    "sourceName": "Jazz_2024-Apr-10_07-23-28PM-000_CustomizedView43689391694_jpg.jpg",
+    "local": "assets/renders/19-jazz-2024-apr-10-07-23-28pm-000-customizedview43689391694-jpg.jpg",
+    "bytes": 603241
+  },
+  {
+    "sourceName": "Jazz_2025-May-04_03-41-21AM-000_CustomizedView16342859476_jpg.jpg",
+    "local": "assets/renders/20-jazz-2025-may-04-03-41-21am-000-customizedview16342859476-jpg.jpg",
+    "bytes": 681391
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-20-47AM-000_CustomizedView3908991307_jpg.jpg",
+    "local": "assets/renders/21-revision-zero-2025-jul-12-06-20-47am-000-customizedview3908991307-jpg.jpg",
+    "bytes": 283261
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-34-20AM-000_CustomizedView12647960830_jpg.jpg",
+    "local": "assets/renders/22-revision-zero-2025-jul-12-06-34-20am-000-customizedview12647960830-jpg.jpg",
+    "bytes": 403901
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-37-45AM-000_CustomizedView4558678813_jpg.jpg",
+    "local": "assets/renders/23-revision-zero-2025-jul-12-06-37-45am-000-customizedview4558678813-jpg.jpg",
+    "bytes": 418669
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-39-40AM-000_CustomizedView8583869675_jpg.jpg",
+    "local": "assets/renders/24-revision-zero-2025-jul-12-06-39-40am-000-customizedview8583869675-jpg.jpg",
+    "bytes": 334700
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-47-42AM-000_CustomizedView9845252265_jpg.jpg",
+    "local": "assets/renders/25-revision-zero-2025-jul-12-06-47-42am-000-customizedview9845252265-jpg.jpg",
+    "bytes": 438134
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-51-51AM-000_CustomizedView21696385589_jpg.jpg",
+    "local": "assets/renders/26-revision-zero-2025-jul-12-06-51-51am-000-customizedview21696385589-jpg.jpg",
+    "bytes": 643186
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-52-49AM-000_CustomizedView31970014299_jpg.jpg",
+    "local": "assets/renders/27-revision-zero-2025-jul-12-06-52-49am-000-customizedview31970014299-jpg.jpg",
+    "bytes": 726117
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-53-52AM-000_CustomizedView13839755552_jpg.jpg",
+    "local": "assets/renders/28-revision-zero-2025-jul-12-06-53-52am-000-customizedview13839755552-jpg.jpg",
+    "bytes": 332045
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-54-49AM-000_CustomizedView11240437429_jpg.jpg",
+    "local": "assets/renders/29-revision-zero-2025-jul-12-06-54-49am-000-customizedview11240437429-jpg.jpg",
+    "bytes": 276490
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-56-53AM-000_CustomizedView15274930050_jpg.jpg",
+    "local": "assets/renders/30-revision-zero-2025-jul-12-06-56-53am-000-customizedview15274930050-jpg.jpg",
+    "bytes": 272465
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_06-57-44AM-000_CustomizedView3429998601_jpg.jpg",
+    "local": "assets/renders/31-revision-zero-2025-jul-12-06-57-44am-000-customizedview3429998601-jpg.jpg",
+    "bytes": 449641
+  },
+  {
+    "sourceName": "REVISION_ZERO_2025-Jul-12_07-07-09AM-000_CustomizedView4820798692_jpg.jpg",
+    "local": "assets/renders/32-revision-zero-2025-jul-12-07-07-09am-000-customizedview4820798692-jpg.jpg",
+    "bytes": 319068
+  },
+  {
+    "sourceName": "Vasuki_2024-Aug-08_10-55-22PM-000_CustomizedView1636563838_jpg.jpg",
+    "local": "assets/renders/33-vasuki-2024-aug-08-10-55-22pm-000-customizedview1636563838-jpg.jpg",
+    "bytes": 640306
+  },
+  {
+    "sourceName": "disarm_end_effector_2025-May-09_05-57-54PM-000_CustomizedView3645279637_jpg.jpg",
+    "local": "assets/renders/34-disarm-end-effector-2025-may-09-05-57-54pm-000-customizedview3645279637-.jpg",
+    "bytes": 123086
+  },
+  {
+    "sourceName": "disarm_end_effector_2025-May-11_05-54-44AM-000_CustomizedView30033875095_jpg.jpg",
+    "local": "assets/renders/35-disarm-end-effector-2025-may-11-05-54-44am-000-customizedview30033875095.jpg",
+    "bytes": 340166
+  },
+  {
+    "sourceName": "disarm_end_effector_2025-May-11_05-55-08AM-000_CustomizedView25206565222_jpg.jpg",
+    "local": "assets/renders/36-disarm-end-effector-2025-may-11-05-55-08am-000-customizedview25206565222.jpg",
+    "bytes": 234385
+  }
+];
